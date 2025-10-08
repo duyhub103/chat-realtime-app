dae19711c098f2aac47b01459af0d0bf1c1d7433
duyhub103 <haogduy12345@gmail.com>
2025-10-08 23:39:41 +0700

    feat: implement login screen UI and Firebase authentication

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,10 +1,11 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.google.gms.google.services)
 }
 
 android {
     namespace = "com.example.chat_realtime_app"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.chat_realtime_app"
@@ -37,7 +38,13 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.firebase.firestore)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation(libs.ccp)
+
+    implementation(platform("com.google.firebase:firebase-bom:34.3.0"))
+    implementation("com.google.firebase:firebase-auth")
+
 }
---
+++ app/src/main/java/com/example/chat_realtime_app/utils/AndroidUtil.java
@@ -0,0 +1,5 @@
+package com.example.chat_realtime_app.utils;
+
+public class AndroidUtil {
+
+}
