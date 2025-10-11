8be631c135c023351ab0201b7dc2ab5d84e78808
duyhub103 <haogduy12345@gmail.com>
2025-10-12 01:12:20 +0700

    feat: implement update profile user - logout

--- app/src/main/java/com/example/chat_realtime_app/ProfileFragment.java
+++ app/src/main/java/com/example/chat_realtime_app/ProfileFragment.java
@@ -1,43 +1,108 @@
 package com.example.chat_realtime_app;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
-import android.view.View;
+import android.view.*;
 import android.view.ViewGroup;
+import android.widget.*;
+import android.widget.ImageView;
+
+import com.example.chat_realtime_app.model.UserModel;
+import com.example.chat_realtime_app.utils.AndroidUtil;
+import com.example.chat_realtime_app.utils.FirebaseUtil;
+import com.google.android.gms.tasks.OnCompleteListener;
 
 public class ProfileFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    ImageView profilePic;
+    EditText usernameInput;
+    EditText phoneInput;
+    Button updateProfileBtn;
+    ProgressBar progressBar;
+    TextView logoutBtn;
+    UserModel currentUserModel;
 
     public ProfileFragment() {
         // Required empty public constructor
     }
 
 
-    // TODO: Rename and change types and number of parameters
-    public static ProfileFragment newInstance(String param1, String param2) {
-        ProfileFragment fragment = new ProfileFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        profilePic = view.findViewById(R.id.profile_image_view);
+        usernameInput = view.findViewById(R.id.profile_username);
+        phoneInput = view.findViewById(R.id.profile_phone);
+        updateProfileBtn = view.findViewById(R.id.profle_update_btn);
+        progressBar = view.findViewById(R.id.profile_progress_bar);
+        logoutBtn = view.findViewById(R.id.logout_btn);
+
+        getUserData();
+
+        updateProfileBtn.setOnClickListener(v -> {
+            updateBtnClick();
+        });
+
+        logoutBtn.setOnClickListener(v -> {
+            FirebaseUtil.logout();
+            Intent intent = new Intent(getContext(), SplashActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+        });
+
+
+
+        return view;
+    }
+
+    void updateBtnClick(){
+        String newUsername = usernameInput.getText().toString();
+        if(newUsername.isEmpty() || newUsername.length()<3){
+            usernameInput.setError("Username must be at least 3 characters chars");
+            usernameInput.requestFocus();
+            return;
+        }
+        currentUserModel.setUsername(newUsername);
+        setInProgress(true);
+        updateToFirestore();
+
+    }
+
+    //save to database
+    void updateToFirestore(){
+        FirebaseUtil.currentUserDetails().set(currentUserModel)
+                .addOnCompleteListener(task -> {
+                    setInProgress(false);
+                    if(task.isSuccessful()){
+                        AndroidUtil.showToast(getContext(), "Updated Successfully");
+                    }else{
+                        AndroidUtil.showToast(getContext(), "Updated Failed");
+                    }
+                });
+    }
+
+    void getUserData(){
+        setInProgress(true);
+        FirebaseUtil.currentUserDetails().get().addOnCompleteListener(task -> {
+            setInProgress(false);
+           currentUserModel = task.getResult().toObject(UserModel.class);
+           usernameInput.setText(currentUserModel.getUsername());
+           phoneInput.setText(currentUserModel.getPhone());
+        });
+    }
+
+    void setInProgress(boolean inProgress){
+        if(inProgress){
+            progressBar.setVisibility(View.VISIBLE);
+            updateProfileBtn.setVisibility(View.GONE);
+        }else{
+            progressBar.setVisibility(View.GONE);
+            updateProfileBtn.setVisibility(View.VISIBLE);
+        }
     }
 }
